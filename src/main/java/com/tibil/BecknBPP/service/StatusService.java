5490c5b5fc0766796ac730bf3a2c37a902c9d267
ShashankGBN <shashank.gowda@tibilsolutions.com>
2023-04-24 15:58:05 +0530

    changes in update , status service

--- src/main/java/com/tibil/BecknBPP/service/StatusService.java
+++ src/main/java/com/tibil/BecknBPP/service/StatusService.java
@@ -14,13 +14,14 @@ package com.tibil.BecknBPP.service;
 	import com.tibil.BecknBPP.dto.Circle;
 	import com.tibil.BecknBPP.dto.Contact;
 	import com.tibil.BecknBPP.dto.Context.ActionEnum;
+import com.tibil.BecknBPP.dto.Order.StateEnum;
 import com.tibil.BecknBPP.dto.Descriptor;
 import com.tibil.BecknBPP.dto.Fulfillment;
 	import com.tibil.BecknBPP.dto.FulfillmentStart;
 	import com.tibil.BecknBPP.dto.InlineResponse2001;
 	import com.tibil.BecknBPP.dto.Item;
 	import com.tibil.BecknBPP.dto.Location;
-import com.tibil.BecknBPP.dto.OnConfirmBody;
+import com.tibil.BecknBPP.dto.OnStatusBody;
 import com.tibil.BecknBPP.dto.Order;
 import com.tibil.BecknBPP.dto.Payment;
 import com.tibil.BecknBPP.dto.Payment.StatusEnum;
@@ -31,7 +32,7 @@ import com.tibil.BecknBPP.dto.Quotation;
 	import com.tibil.BecknBPP.dto.QuotationBreakup;
 import com.tibil.BecknBPP.dto.SelectMessage;
 import com.tibil.BecknBPP.dto.State;
-import com.tibil.BecknBPP.dto.ConfirmBody;
+import com.tibil.BecknBPP.dto.StatusBody;
 	import com.tibil.BecknBPP.model.Candidate;
 	import com.tibil.BecknBPP.model.ServiceRequestFlow;
 
@@ -51,12 +52,12 @@ import com.tibil.BecknBPP.dto.ConfirmBody;
 		@Override
 		public void processInternalRequest(Object requestBody) {
 
-			ConfirmBody inputBody = (ConfirmBody) requestBody;
+			StatusBody inputBody = (StatusBody) requestBody;
 
-			String confirmRequestSerialized = utils.getSerialisedData(inputBody);
-			dbUtils.insertServiceRequest(inputBody.getContext(), confirmRequestSerialized);
+			String statusRequestSerialized = utils.getSerialisedData(inputBody);
+			dbUtils.insertServiceRequest(inputBody.getContext(), statusRequestSerialized);
 
-			dbUtils.insertRequestFlow(inputBody.getContext(), confirmRequestSerialized, null);
+			dbUtils.insertRequestFlow(inputBody.getContext(), statusRequestSerialized, null);
 
 		}
 
@@ -64,16 +65,18 @@ import com.tibil.BecknBPP.dto.ConfirmBody;
 		@Scheduled(fixedDelay = Constants.SCHEDULE_DELAY)
 		public void processCallbackRequest() {
 
-			List<ServiceRequestFlow> flows = dbUtils.getRequestsOnTimeAndAction(ActionEnum.CONFIRM.toString());
+			List<ServiceRequestFlow> flows = dbUtils.getRequestsOnTimeAndAction(ActionEnum.STATUS.toString());
 			for (ServiceRequestFlow f : flows) {
 				System.out.println("Response from DB");
 				System.out.println(f.toString());
 
-				ConfirmBody confirmBody = utils.deserialiseData(f.getData(), ConfirmBody.class);
+				StatusBody statusBody = utils.deserialiseData(f.getData(), StatusBody.class);
 
+				String orderId = getStatusOrderId(statusBody);
+				System.out.println("Order ID --------------" + orderId);
 				
-				OnConfirmBody body = getOnConfirmBody(confirmBody);
-				ResponseEntity<InlineResponse2001> response = restTemplate.postForEntity("http://localhost:8080/on_confirm",
+				OnStatusBody body = getOnStatusBody(statusBody);
+				ResponseEntity<InlineResponse2001> response = restTemplate.postForEntity("http://localhost:8080/on_status",
 						body, InlineResponse2001.class);
 
 				dbUtils.insertRequestFlow(body.getContext(), utils.getSerialisedData(body),
@@ -84,30 +87,45 @@ import com.tibil.BecknBPP.dto.ConfirmBody;
 		}
 
 		
-		public String getConfirmPayment(ConfirmBody confirmBody) {
+		public String getStatusOrderId(StatusBody statusBody) {
 
-			return confirmBody.getMessage().getOrder().getPayment().getUri();
+			return statusBody.getMessage().getOrderId();
 		}
 
 
-		public OnConfirmBody getOnConfirmBody(ConfirmBody confirmBody) {
+		public OnStatusBody getOnStatusBody(StatusBody statusBody) {
 
-			OnConfirmBody onConfirmBody = new OnConfirmBody();
-			onConfirmBody.setContext(confirmBody.getContext().action(ActionEnum.ON_CONFIRM));
-			onConfirmBody.setMessage(new SelectMessage());
-			onConfirmBody.getMessage().setOrder(new Order().id("order_id-001").provider(new Provider().id("Tibil solutions")));
+			OnStatusBody onStatusBody = new OnStatusBody();
+			onStatusBody.setContext(statusBody.getContext().action(ActionEnum.ON_STATUS));
+			onStatusBody.setMessage(new SelectMessage());
+			onStatusBody.getMessage().setOrder(new Order().id("order_id-001").state(StateEnum.ACTIVE).provider(new Provider().id("Tibil solutions")));
 
-			Item items = new Item().id("EMP-123");
+			Item items = new Item().id("1");
+			
+			
 			
 			Billing billing = new Billing().phone("9620336606").name("Sanjay").email("sanjay@peppo.co.in");
+			
+			onStatusBody.getMessage().getOrder().billing(billing);
 			
 			Fulfillment fulFillment = new Fulfillment().state(new State().descriptor(new Descriptor().name("SEARCHING-FOR-FMD-AGENT")));
 			fulFillment.tracking(false).start(new FulfillmentStart().location(new Location().id("Tibil solutions").circle(new Circle().gps("12.9423184,77.6016338"))));
 			fulFillment.getEnd().contact(new Contact().phone("9620336606")).person(new Person().name("Sanjay")).location(new Location().gps("12.964319, 77.6810060000001"));
 			
-			onConfirmBody.getMessage().setOrder(new Order().quote(new Quotation().price(new Price().currency("INR").value("30")).breakup((List<QuotationBreakup>) new QuotationBreakup())));               //.title("Employee name").price(new Price().value("10.0")))));
-			onConfirmBody.getMessage().setOrder(new Order().payment(new Payment().status(StatusEnum.PAID)));
-			return onConfirmBody;
+			onStatusBody.getMessage().getOrder().fulfillment(fulFillment);
+			onStatusBody.getMessage().getOrder().quote(new Quotation().price(new Price().currency("INR").value("30")));
+
+			
+			QuotationBreakup quotationBreakup = new QuotationBreakup().title("Employee name").price(new Price().value("10"));
+			quotationBreakup.title("Employee name200").price(new Price().value("10"));
+			quotationBreakup.title("BPP Fee").price(new Price().value("5"));
+			quotationBreakup.title("BAP Fee").price(new Price().value("5"));
+			
+			onStatusBody.getMessage().getOrder().setQuote(new Quotation().addBreakupItem(quotationBreakup));
+			
+			onStatusBody.getMessage().setOrder(new Order().payment(new Payment().params(null)));
+			onStatusBody.getMessage().setOrder(new Order().payment(new Payment().status(StatusEnum.PAID)));
+			return onStatusBody;
 		}
 	
 }
--- src/main/java/com/tibil/BecknBPP/service/UpdateService.java
+++ src/main/java/com/tibil/BecknBPP/service/UpdateService.java
@@ -20,7 +20,7 @@ import com.tibil.BecknBPP.dto.FulfillmentStart;
 import com.tibil.BecknBPP.dto.InlineResponse2001;
 import com.tibil.BecknBPP.dto.Item;
 import com.tibil.BecknBPP.dto.Location;
-import com.tibil.BecknBPP.dto.OnConfirmBody;
+import com.tibil.BecknBPP.dto.OnUpdateBody;
 import com.tibil.BecknBPP.dto.Order;
 import com.tibil.BecknBPP.dto.Order.StateEnum;
 import com.tibil.BecknBPP.dto.Payment;
@@ -32,7 +32,7 @@ import com.tibil.BecknBPP.dto.Quotation;
 import com.tibil.BecknBPP.dto.QuotationBreakup;
 import com.tibil.BecknBPP.dto.SelectMessage;
 import com.tibil.BecknBPP.dto.State;
-import com.tibil.BecknBPP.dto.ConfirmBody;
+import com.tibil.BecknBPP.dto.UpdateBody;
 import com.tibil.BecknBPP.model.Candidate;
 import com.tibil.BecknBPP.model.ServiceRequestFlow;
 
@@ -52,12 +52,12 @@ public class UpdateService implements ProcessInternalRequestService {
 	@Override
 	public void processInternalRequest(Object requestBody) {
 
-		ConfirmBody inputBody = (ConfirmBody) requestBody;
+		UpdateBody inputBody = (UpdateBody) requestBody;
 
-		String confirmRequestSerialized = utils.getSerialisedData(inputBody);
-		dbUtils.insertServiceRequest(inputBody.getContext(), confirmRequestSerialized);
+		String updateRequestSerialized = utils.getSerialisedData(inputBody);
+		dbUtils.insertServiceRequest(inputBody.getContext(), updateRequestSerialized);
 
-		dbUtils.insertRequestFlow(inputBody.getContext(), confirmRequestSerialized, null);
+		dbUtils.insertRequestFlow(inputBody.getContext(), updateRequestSerialized, null);
 
 	}
 
@@ -65,18 +65,15 @@ public class UpdateService implements ProcessInternalRequestService {
 	@Scheduled(fixedDelay = Constants.SCHEDULE_DELAY)
 	public void processCallbackRequest() {
 
-		List<ServiceRequestFlow> flows = dbUtils.getRequestsOnTimeAndAction(ActionEnum.CONFIRM.toString());
+		List<ServiceRequestFlow> flows = dbUtils.getRequestsOnTimeAndAction(ActionEnum.UPDATE.toString());
 		for (ServiceRequestFlow f : flows) {
 			System.out.println("Response from DB");
 			System.out.println(f.toString());
 
-			ConfirmBody confirmBody = utils.deserialiseData(f.getData(), ConfirmBody.class);
-			StateEnum orderState = getStatus(confirmBody);
-
-			System.out.println("State of order --------------" + orderState);
-
-			OnConfirmBody body = getOnConfirmBody(confirmBody);
-			ResponseEntity<InlineResponse2001> response = restTemplate.postForEntity("http://localhost:8080/on_confirm",
+			UpdateBody updateBody = utils.deserialiseData(f.getData(), UpdateBody.class);
+		
+			OnUpdateBody body = getOnUpdateBody(updateBody);
+			ResponseEntity<InlineResponse2001> response = restTemplate.postForEntity("http://localhost:8080/on_update",
 					body, InlineResponse2001.class);
 
 			dbUtils.insertRequestFlow(body.getContext(), utils.getSerialisedData(body),
@@ -86,32 +83,36 @@ public class UpdateService implements ProcessInternalRequestService {
 
 	}
 
-	
-	public StateEnum getStatus(ConfirmBody confirmBody) {
 
-		return confirmBody.getMessage().getOrder().getState();
-	}
+	public OnUpdateBody getOnUpdateBody(UpdateBody updateBody) {
 
+		OnUpdateBody onUpdateBody = new OnUpdateBody();
+		onUpdateBody.setContext(updateBody.getContext().action(ActionEnum.ON_UPDATE));
+		onUpdateBody.setMessage(new SelectMessage());
+		onUpdateBody.getMessage().setOrder(new Order().id("order_id-001").state(StateEnum.CANCELLED).provider(new Provider().id("Tibil solutions")));
 
-	public OnConfirmBody getOnConfirmBody(ConfirmBody confirmBody) {
-
-		OnConfirmBody onConfirmBody = new OnConfirmBody();
-		onConfirmBody.setContext(confirmBody.getContext().action(ActionEnum.ON_CONFIRM));
-		onConfirmBody.setMessage(new SelectMessage());
-		onConfirmBody.getMessage().setOrder(new Order().id("order_id-001").state(StateEnum.CANCELLED).provider(new Provider().id("Tibil solutions")));
-
-		Item items = new Item().id("EMP-200");
+		Item items = new Item().id("1");
+		
 		
 		Billing billing = new Billing().phone("9620336606").name("Sanjay").email("sanjay@peppo.co.in");
+		
+		onUpdateBody.getMessage().getOrder().billing(billing);
 		
 		Fulfillment fulFillment = new Fulfillment().state(new State().descriptor(new Descriptor().name("SEARCHING-FOR-FMD-AGENT")));
 		fulFillment.tracking(false).start(new FulfillmentStart().location(new Location().id("Tibil solutions").circle(new Circle().gps("12.9423184,77.6016338"))));
 		fulFillment.getEnd().contact(new Contact().phone("9620336606")).person(new Person().name("Sanjay")).location(new Location().gps("12.964319, 77.6810060000001"));
 		
-		onConfirmBody.getMessage().setOrder(new Order().quote(new Quotation().price(new Price().currency("INR").value("30"))));
-		onConfirmBody.getMessage().setOrder(new Order().payment(new Payment().status(StatusEnum.PAID)));
+		onUpdateBody.getMessage().setOrder(new Order().quote(new Quotation().price(new Price().currency("INR").value("20"))));
 		
-		return onConfirmBody;
+		QuotationBreakup quotationBreakup = new QuotationBreakup().title("Employee name200").price(new Price().value("10"));
+		quotationBreakup.title("BPP Fee").price(new Price().value("5"));
+		quotationBreakup.title("BAP Fee").price(new Price().value("5"));
+		
+		onUpdateBody.getMessage().getOrder().setQuote(new Quotation().addBreakupItem(quotationBreakup));
+		
+		onUpdateBody.getMessage().setOrder(new Order().payment(new Payment().status(StatusEnum.PAID)));
+		
+		return onUpdateBody;
 	}
 
 }
